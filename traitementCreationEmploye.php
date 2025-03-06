0db20623fc17d6b2d8d5b95b85d965aebb1e9595
mfa008 <143124567+mfa008@users.noreply.github.com>
2025-03-06 15:57:40 +0000

    modif nom BD

--- traitementCreationEmploye.php
+++ traitementCreationEmploye.php
@@ -3,7 +3,7 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
-        $db = new PDO('mysql:host=localhost;dbname=entreprise;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=smarttech;charset=utf8', 'root', '');
 
         $stmt = $db->prepare("INSERT INTO employes 
             (nom, prenom, email, poste, salaire, date_embauche, departement)
